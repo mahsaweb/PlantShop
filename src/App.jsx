c4987e4073b8d7d68b3eeb10e54732183d8fcab3
Mahsa Fallah <mahsa0fallah@gmail.com>
2026-03-23 19:51:35 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import react  from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+function App() {
+  return (
+    <Swiper
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      // بقیه تنظیمات...
+    >
+      <SwiperSlide>محصول ۱</SwiperSlide>
+      <SwiperSlide>محصول ۲</SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default App;
